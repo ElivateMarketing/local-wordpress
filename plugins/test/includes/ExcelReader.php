253440b9ad5372f3206f307503f616ee9d88e4e6
alex-elivate <60756727+alex-elivate@users.noreply.github.com>
2025-02-27 20:03:40 -0700

    update

--- plugins/test/includes/ExcelReader.php
+++ plugins/test/includes/ExcelReader.php
@@ -5,7 +5,7 @@ namespace Test\Includes;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class ExcelReader {
-    public function readExcelFile($filePath) {
+    public function readExcelFile($filePath): mixed {
         $spreadsheet = IOFactory::load($filePath);
         $sheet = $spreadsheet->getActiveSheet();
         $data = $sheet->toArray(null, true, true, true);
